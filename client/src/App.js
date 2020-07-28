f5e47e5c44d1c1389aad745bc1b44a464c27a969
Cher <red_phoenix417@yahoo.com>
2020-07-28 17:38:25 -0400

    Added test API connect and data.json file on api

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState()
+
+  useEffect(()=> {
+    fetch("http://localhost:5000/test")
+    .then(res => res.text())
+    .then(res => setData(res))
+  },[])
+
+  console.log("data is", data)
   return (
     <div className="App">
       <header className="App-header">
