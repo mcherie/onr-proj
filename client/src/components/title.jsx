42ee599216f1ab968c6a04f10fc7a144ee1a87ad
Cher <49999513+mcherie@users.noreply.github.com>
2020-08-01 01:40:26 -0400

    Added fontawesome globe icon

--- client/src/components/title.jsx
+++ client/src/components/title.jsx
@@ -2,6 +2,9 @@ import React from "react"
 
 import styled from "styled-components"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+
 export const Title = () => {
 
     const Head = styled.div`
@@ -12,6 +15,7 @@ export const Title = () => {
 
     return (
         <Head >
+            <FontAwesomeIcon icon={faGlobe} size="2x"/>
             Diagnostic Tool
         </Head>
     )
