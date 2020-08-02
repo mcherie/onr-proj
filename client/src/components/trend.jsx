64ea9dffad9af3b8566103e5d5ac508e10713c2c
Cher <49999513+mcherie@users.noreply.github.com>
2020-08-02 00:01:43 -0400

    Created selector tab icons on trends

--- client/src/components/trend.jsx
+++ client/src/components/trend.jsx
@@ -2,17 +2,58 @@ import React from "react"
 
 import styled from "styled-components"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDay, faCalendarWeek, faCalendarCheck, faDiceFour, faDiceSix, faGlassCheers,} from '@fortawesome/free-solid-svg-icons'
+
 export const Trend = () => {
 
-    const Box = styled.div`
-        background: blue;
+    // this should be for the whole trend itself, so flex equal with "filters"
+    const Wrapper = styled.div`
         flex: 1;
         text-align: left;
     `
+    const Content = styled.div`
+        background: blue;
+        display: flex;
+    `
+
+
+    const Tab = styled.div`
+        display: flex;
+        justify-content: space-between;
+        flex: 2;
+    `
+
+    const Right = styled.div`
+    flex: 1;
+`
+
+    const Area = styled.div`
+        background: grey;
+        height: 30vh;
+        width: 30vh;
+    `
+
+    const Test = styled.div`
+        flex: 1;
+    `
+
     return (
-        <Box >
-            QUALITY TREND SCORE
-        </Box>
+        <Wrapper >
+            <Content>
+            <Test >QUALITY TREND SCORE</Test>
+            <Tab > 
+                <FontAwesomeIcon icon={faCalendarDay} size="2x"/>
+                <FontAwesomeIcon icon={faCalendarWeek} size="2x"/>
+                <FontAwesomeIcon icon={faCalendarCheck} size="2x"/>
+                <FontAwesomeIcon icon={faDiceFour} size="2x"/>
+                <FontAwesomeIcon icon={faDiceSix} size="2x"/>
+                <FontAwesomeIcon icon={faGlassCheers} size="2x"/>
+            </Tab>
+            <Right />
+            </Content>
+        </Wrapper>
+
     )
 
 }
