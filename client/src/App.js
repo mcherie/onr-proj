37cb9c11bbf5bed72fcd5ed5f0200390add1b13b
Cher <49999513+mcherie@users.noreply.github.com>
2020-08-03 17:10:10 -0400

    Removed commented out codes and unused logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 import { Navbar } from "./components/navbar"
@@ -21,17 +20,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          {data}
-
-        </a> */}
         <Sidebar />
       </header>
       <div className="body">
